b8c90e86b4bca5c04a82fcaf4091dc6fe994d53c
GYANENDRA8080 <gyanendra8080@gmail.com>
2025-07-12 15:53:29 +0530

    basic setcup of django project directorys

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+      # Include the URLs from calculatorapp
     path('admin/', admin.site.urls),
+    path('calculatorapp', include('calculatorapp.urls')),  # Include the URLs from calculatorapp
 ]
